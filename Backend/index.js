ec9027bdd5a76ea0516ac71e4b38c4273d64803f
anandbhardwaj456 <120586775+anandbhardwaj456@users.noreply.github.com>
2025-02-25 01:44:40 +0530

    updated mongo

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 // Making MongoDB Connection with a static URI
-const MONGO_URI = "mongodb+srv://anand193376:Eom4NKGXCcVPKjIM@cluster0.k88tp.mongodb.net/codeeditor";
+const MONGO_URI = "mongodb+srv://anandbhardwaj:anand@123@codeeditor.czwmi.mongodb.net/";
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("✅ MongoDB Connected"))
