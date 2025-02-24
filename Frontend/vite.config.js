c3293864f66456d1f60e784b64a3fc5377c6555a
anandbhardwaj456 <120586775+anandbhardwaj456@users.noreply.github.com>
2025-02-25 00:59:18 +0530

    changed vite js config to deploy

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,5 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  "proxy": "https://codeeditor-1-ocln.onrender.com/"
+  server: {
+    host: true,  // Allows access from network
+    port: 5173,  // Change if needed
+    allowedHosts: ["codeeditor-3-fy6t.onrender.com"],  // Allow Render deployment
+    proxy: {
+      "/api": {
+        target: "https://codeeditor-1-ocln.onrender.com/",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
