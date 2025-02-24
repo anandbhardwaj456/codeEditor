37339057f6d606877833f346a1d6f5f09b23a0e2
anandbhardwaj456 <120586775+anandbhardwaj456@users.noreply.github.com>
2025-02-24 13:43:32 +0530

    renamed login.css->Login.css

--- Frontend/src/components/LoginPage.jsx
+++ Frontend/src/components/LoginPage.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "./login.css";
+import "./Login.css";
 
 const LoginPage = () => {
   const [formData, setFormData] = useState({ email: "", password: "" });
